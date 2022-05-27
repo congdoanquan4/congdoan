179ba2a0de2e16de6143abd8625083aea057e238
tritovinh <nguyenlbfx11517@funix.edu.vn>
2022-05-27 16:19:08 +0700

    more

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Get the button
+//nút quay về đầu trang
 let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
@@ -13,3 +13,27 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// array thông tin 
+let thongTinArray = [
+  {
+    img: "",
+    name:"Hội thao Tháng Công nhân Quận 4 năm 2022",
+  }
+]
+
+// chèn thông tin vô trang thông tin
+let contentThongTin = document.getElementById("content-thongtin")
+function renderThongTin() {
+  for (let i of thongTinArray){
+    contentThongTin.innerHTML += `
+      <div class="content-container" >
+          <a class="content" >
+              <img src="${i.img}" alt="">
+              <p>${i.name}</p>
+          </a>
+      </div>
+    `
+  }
+}
+renderThongTin();
